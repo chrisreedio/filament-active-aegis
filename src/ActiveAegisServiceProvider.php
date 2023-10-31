de0db2f127a465c334b72dbf1cb29229f415a2d0
chrisreedio <chrisreedio@users.noreply.github.com>
2023-10-31 11:52:09 +0000

    Fix styling

--- src/ActiveAegisServiceProvider.php
+++ src/ActiveAegisServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace ChrisReedIO\ActiveAegis;
 
+use ChrisReedIO\ActiveAegis\Commands\ActiveAegisCommand;
+use ChrisReedIO\ActiveAegis\Testing\TestsActiveAegis;
 use Filament\Support\Assets\AlpineComponent;
 use Filament\Support\Assets\Asset;
 use Filament\Support\Assets\Css;
@@ -13,8 +15,6 @@ use Livewire\Features\SupportTesting\Testable;
 use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use ChrisReedIO\ActiveAegis\Commands\ActiveAegisCommand;
-use ChrisReedIO\ActiveAegis\Testing\TestsActiveAegis;
 
 class ActiveAegisServiceProvider extends PackageServiceProvider
 {
